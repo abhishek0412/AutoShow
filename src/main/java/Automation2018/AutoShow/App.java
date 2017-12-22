55df09a2bedd9857c7f936bc7a3ecfce6baf98bb
abhishek0412 <abhishek.choudharry@gmail.com>
2017-12-22 12:55:30 +0530

    Created new AutoShow Project.

---
+++ src/main/java/Automation2018/AutoShow/App.java
@@ -0,0 +1,13 @@
+package Automation2018.AutoShow;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
